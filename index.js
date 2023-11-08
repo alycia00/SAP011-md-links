74b3fab7fc881a74754ebd64f10d63161cee4856
Alycia <alyekaka@gmail.com>
2023-11-08 12:59:16 -0300

    primeiras alteraçoes do video da naroka

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-  // ...
-};
+// exemplo do video da Naroka:
+  const { rejects } = require("assert");
+  const fs = require("fs") // biblioteca do node
+
+  function soma (a, b){
+    return a + b
+  }
+  
+  function lerArquivo(caminhoDoArquivo) {
+    return new Promise(function(resolve, reject) {  // criando uma promise. <= essa é a estrutura 
+    fs.readFile(caminhoDoArquivo, "utf8", (err, data) =>  {  // readFile é uma funçao assincrona
+      if(err) reject();
+      resolve(data)
+    });
+  });
+}
+module.exports = { soma, lerArquivo };
